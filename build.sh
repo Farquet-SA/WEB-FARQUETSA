6dc61cba449121d131bd6b4e6fb2c99973caa986
Farquet-SA <farquetsaweb@outlook.com>
2026-05-08 01:05:15 -0600

    fix: update build.sh to use uv

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 set -e
 
 echo "==> Instalando dependencias..."
-pip install -r BAC/requirements.txt
+uv pip install -r BAC/requirements.txt --system --break-system-packages
 
 echo "==> Corriendo migraciones..."
 cd BAC && python manage.py migrate --noinput
 
 echo "==> Colectando archivos estáticos..."
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinputv
